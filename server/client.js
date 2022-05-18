5195c72189a4d8014c58b05462d12104d60a6dd3
Avery Black <avery.c.black@gmail.com>
2022-05-17 19:53:43 -0700

    Simplify

--- server/client.js
+++ server/client.js
@@ -3,42 +3,24 @@ const dgram = require("node:dgram");
 const { argv } = require("node:process");
 const socket = dgram.createSocket("udp4");
 
-const GET_ROLE = 0;
 const GET_IP = 1;
 const GET_MESSAGE = 2;
 
-const ROLE_HOST = 0;
-const ROLE_CLIENT = 1;
-
-let role;
 let otherClient;
-let state = GET_ROLE;
-
-socket.on('error', err => {
-  console.error(`client error:\n${err.stack}`);
-  socket.close();
-});
+let state = GET_IP;
 
 let index = 0;
 
-socket.on('message', async (msg, rinfo) => {
+socket.on('message', (msg, rinfo) => {
   switch (state) {
-    // Are we host sending info (stream) or client receiving?
-    case GET_ROLE:
-      if (msg == "Client1") role = ROLE_HOST;
-      else if (msg == "Client2") role = ROLE_CLIENT;
-      state = GET_IP;
-      break;
     // Get IP of the other device we are talking too.
     case GET_IP:
       otherClient = JSON.parse(msg);
 
-      // Print other client info, and role we play
-      const roleStr = role ? "Client" : "Host";
-      console.log(`Role: ${roleStr}, Other IP: ${otherClient.address}:${otherClient.port}`);
+      // Print other client info
+      console.log(`Other IP: ${otherClient.address}:${otherClient.port}`);
 
       socket.send("Hello World! I hope you get this....", otherClient.port, otherClient.address);
-      console.log("Sent UDP Packet to other IP!");
       state = GET_MESSAGE;
       break;
     case GET_MESSAGE:
@@ -50,12 +32,8 @@ socket.on('message', async (msg, rinfo) => {
           socket.close();
         }
       });
+      break;
   }
-});
-
-socket.on('listening', () => {
-  const address = socket.address();
-  console.log(`client listening ${address.address}:${address.port}`);
 });
 
 if (argv.length < 4) {
--- server/server.js
+++ server/server.js
@@ -1,11 +1,6 @@
 const dgram = require("node:dgram");
 const server = dgram.createSocket("udp4");
 
-server.on('error', err => {
-    console.error(`server error:\n${err.stack}`);
-    server.close();
-});
-
 let client1 = null;
 let client2 = null;
 let numClients = 0;
@@ -14,12 +9,10 @@ server.on('message', (msg, rinfo) => {
     console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
     if (numClients == 0) {
         client1 = rinfo;
-        server.send("Client1", rinfo.port, rinfo.address);
         numClients++;
     } else {
         client2 = rinfo;
         numClients = 0;
-        server.send("Client2", rinfo.port, rinfo.address);
         
         server.send(JSON.stringify(client2), client1.port, client1.address);
         server.send(JSON.stringify(client1), client2.port, client2.address);
@@ -31,4 +24,4 @@ server.on('listening', () => {
   console.log(`server listening ${address.address}:${address.port}`);
 });
 
-server.bind(30000);
+server.bind(25566);

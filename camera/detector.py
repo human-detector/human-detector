dc481dda0485bf6ba6417efb3e6f335589504074
Avery Black <avery.c.black@gmail.com>
2022-05-26 17:15:39 -0700

    Python

---
+++ camera/detector.py
@@ -0,0 +1,7 @@
+
+class Detector:
+    def __init__():
+        pass
+
+    def detect(frame):
+        return []
---
+++ camera/socket.py
@@ -0,0 +1,8 @@
+class ServerConnection:
+    def __init__():
+        pass
+    
+    def send_notification(frame):
+        pass
+
+    def send_snapshot(frame):

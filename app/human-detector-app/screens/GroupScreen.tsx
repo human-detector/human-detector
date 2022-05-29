ea67c7562390e89cdd8d0263a2d9157097835094
latanam <latanam@wwu.edu>
2022-05-29 02:49:27 -0700

    Added screen tsx files

---
+++ app/human-detector-app/screens/GroupScreen.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { View, Text } from 'react-native';
+
+export default function GroupScreen(){
+    
+}
+
+export function GroupOnPress(){
+    return (
+        <View>
+            <Text> Placeholder </Text>
+        </View>
+    )
+}
+
+export function GroupDisplayButton(){
+    return (
+        <View>
+            <Text> Placeholder </Text>
+        </View>
+    )
+}

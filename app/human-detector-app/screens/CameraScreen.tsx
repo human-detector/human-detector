c91c67916be38751a09c701a0ad930d19100a098
latanam <latanam@wwu.edu>
2022-05-29 03:44:24 -0700

    Updated class, notif, and settings screens

---
+++ app/human-detector-app/screens/CameraScreen.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+
+export default function CameraScreen(){
+
+}
+
+export function CameraOnPress(){
+
+}
+
+export function CameraDisplayButton(){
+    
+}
---
+++ app/human-detector-app/screens/NotifScreen.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+
+export default function NotifScreen(){
+
+}
+
+export function ListNotifs(){
+
+}
+
+class Notifications{
+    
+}

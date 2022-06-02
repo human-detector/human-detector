820c73a52c17e77d36daa59bfef4e710147cf6bc
latanam <latanam@wwu.edu>
2022-06-02 01:37:06 -0700

    implemented tests with camera.tsx, notification.tsx, and group.tsx

--- app/human-detector-app/App.tsx
+++ app/human-detector-app/App.tsx
@@ -2,56 +2,17 @@ import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Alert, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  const cameraObj:Camera = new Camera("testing things over here", "3826847623784923")
-
+export default function App(): React.ReactElement {
   return (
-    <View style={styles.container}>
+    <View style={styles2}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-      <ButtonTest cameraName = "Camera 1"/>
-      <ButtonTest cameraName = "Camera 2"/>
-      <ButtonTestTwo cameraName = "Camera 3" function = {testFunction} ></ButtonTestTwo>
-      <DisplayCameraButton camera={ cameraObj } />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-function ButtonTest({ cameraName }) {
-  
-  return (
-    <View>
-      <Button 
-      onPress = {() => 
-        Alert.alert(`Why does this not work`)
-      }
-      title = {cameraName}
-      />
-    </View>
-  )
-}
-
-const testFunction = () => {
-  Alert.alert(`This is the testFunction alert`)
-}
-
-function ButtonTestTwo({ cameraName, function2 }){
-  return(
-    <View>
-      <TouchableOpacity onPress={() => { Alert.alert(`Whatt about this`)}} style = {styles2.appButtonContainer}>
-        <Text style={styles2.appButtonText}>{cameraName}</Text>  
-      </TouchableOpacity>
-    </View>
-  )
+export function CameraListener(){
+  //When there is a signal, get notification information
 }
 
 const styles2 = StyleSheet.create({
@@ -71,26 +32,3 @@ const styles2 = StyleSheet.create({
     textTransform: "uppercase"
   }
 });
-
-
-class Camera {
-  cameraName:string;
-  cameraID:string;
-
-  constructor(cameraName:string, cameraID:string){
-    this.cameraName = cameraName
-    this.cameraID = cameraID
-  }
-}
-
-export function DisplayCameraButton({ camera }:MessageProp){
-  return (
-  <View>
-    <Button 
-    title = {camera.cameraName}
-    onPress = {() => {
-      Alert.alert(`Does this work`)
-    }}
-    />
-  </View>
-)};
--- app/human-detector-app/screens/CameraScreen.tsx
+++ app/human-detector-app/screens/CameraScreen.tsx
@@ -13,3 +13,31 @@ export function CameraOnPress(){
 export function CameraDisplayButton(){
     
 }
+
+export function getCameraThumbnail(){
+
+}
+
+export function getCameraStream(){
+
+}
+
+export function isCameraOnline(): boolean{
+    return true
+}
+
+export class Camera {
+  cameraName:string;
+  cameraID:string;
+  
+  constructor(cameraName:string, cameraID:string){
+    this.cameraName = cameraName
+    this.cameraID = cameraID
+    console.log(cameraName);
+    console.log(cameraID);
+  }
+}
+
+//module.exports = isCameraOnline;
+
+  

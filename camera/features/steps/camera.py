fb9077edeacf9380735a1ae83b14c3d5cf4ef529
Avery Black <avery.c.black@gmail.com>
2022-06-03 15:59:25 -0700

    more tests

---
+++ camera/features/steps/camera.py
@@ -0,0 +1,1 @@
+from behave import *
--- camera/src/socket.py
+++ camera/src/socket.py
@@ -1,11 +1,18 @@
+import requests
+
 class ServerConnection:
-    def __init__():
+    def __init__(self, token, eyespy_api):
+        self.token = token
+        self.api = eyespy_api
         pass
     
     def send_notification(frame):
         pass
 
     def send_snapshot(frame):
+        pass
+
+    def get_settings():
         pass
 
     

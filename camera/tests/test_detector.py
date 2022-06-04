3dd93402e74a7ba22b1bb157b5503e9bf91359b5
Avery Black <avery.c.black@gmail.com>
2022-06-03 18:40:33 -0700

    Pythonify Part 2 (#5)
    * Python
    
    * Python tests?
    
    * Requirements I think?
    
    * Remove cache
    
    * more tests
    
    * behave
    
    * Documentation

--- camera/tests/test_detector.py
+++ camera/tests/test_detector.py
@@ -6,27 +6,24 @@ from src.detector import Detector
 class TestDetector(unittest.TestCase):
     def setUp(self):
         self.detector = Detector()
+        self.dirname = os.path.realpath('.')
 
     def test_detector_blind(self):
-        dirname = os.path.realpath('.')
-        image = cv2.imread(os.path.join(dirname, "tests/images/flashlight.jpg"))
+        image = cv2.imread(os.path.join(self.dirname, "tests/images/flashlight.jpg"))
         output = self.detector.detect(image)
         self.assertTrue(len(output) == 0)
 
     def test_detector_darkness(self):
-        dirname = os.path.realpath('.')
-        image = cv2.imread(os.path.join(dirname, "tests/images/darkness_my_old_friend.jpg"))
+        image = cv2.imread(os.path.join(self.dirname, "tests/images/darkness_my_old_friend.jpg"))
         output = self.detector.detect(image)
         self.assertTrue(len(output) == 0)
 
     def test_detector_no_human(self):
-        dirname = os.path.realpath('.')
-        image = cv2.imread(os.path.join(dirname, "tests/images/no_human.jpg"))
+        image = cv2.imread(os.path.join(self.dirname, "tests/images/no_human.jpg"))
         output = self.detector.detect(image)
         self.assertTrue(len(output) == 0)
         
-    def test_detector_no_human(self):
-        dirname = os.path.realpath('.')
-        image = cv2.imread(os.path.join(dirname, "tests/images/human.jpg"))
+    def test_detector_human(self):
+        image = cv2.imread(os.path.join(self.dirname, "tests/images/human.jpg"))
         output = self.detector.detect(image)
         self.assertTrue(len(output) == 1)

69c77496a7e422932500865fbf92911337bd82a2
latanam <latanam@wwu.edu>
2022-10-02 02:21:17 -0700

    nit comment changes

--- app/human-detector-app/classes/Notification.ts
+++ app/human-detector-app/classes/Notification.ts
@@ -5,17 +5,16 @@ import Camera from './Camera';
  * describes a notification that was sent to a user.  These notifcation
  * will only be notification that are sent from cameras.  Other possible
  * notifications (if any) do not apply to the notification history.
- *
- * notifDate: The date and time that the notificfation was sent.
- * notifSentFrom: The camera that sent the specific notification
- *
- *
  */
 export default class Notification {
   notifDate: string;
 
   notifSentFrom: Camera;
 
+  /**
+   * @param notifDate: The date and time that the notificfation was sent.
+   * @param notifSentFrom: The camera that sent the specific notification
+   */
   constructor(notifDate: string, notifSentFrom: Camera) {
     this.notifDate = notifDate;
     this.notifSentFrom = notifSentFrom;

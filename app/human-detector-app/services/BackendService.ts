952331f9045fdfe2965328422bd2fe72a9993d6d
latanam <latanam@wwu.edu>
2022-10-02 02:17:59 -0700

    Added comments to major changes files

--- app/human-detector-app/services/BackendService.ts
+++ app/human-detector-app/services/BackendService.ts
@@ -6,7 +6,18 @@ import User from '../classes/User';
 import Notification from '../classes/Notification';
 import * as ServerUrl from '../config/ServerConfig';
 
-// Get group endpoint
+/**
+ * This file handles HTTP requests to our backend
+ */
+
+/**
+ * This method is used to get the group list that is connected to a user.  This group list
+ * will contain cameras and other information that will be stored as a group object.
+ *
+ * @param userId : the userId of the user that we need the group list for.
+ * @returns null if error occurs.
+ *          An array of Groups that are owned by the user with the userId.
+ */
 export async function getGroupListAPI(userId: string): Promise<Group[] | null> {
   const apiLinkWithExtension: string =
     ServerUrl.apiLink + ServerUrl.getGroupsListUrlExtension(userId);
@@ -35,9 +46,14 @@ export async function getGroupListAPI(userId: string): Promise<Group[] | null> {
   return testData;
 }
 
-// Send notification key
-// PUT /users/<uuid>/notifyToken
-// 200 ok 401 bad
+/**
+ * This method will send a notification token to the backend to be stored
+ * through our endpoints.  This is done at login.
+ *
+ * @param userIdFromLogin : userId of the user that just logged in
+ * @param expoTokenFromLogin : notification token of the user that just logged in
+ * @returns '200' string if success, else it will return the status code that backend responds with.
+ */
 export async function sendNotifyTokenAPI(
   userIdFromLogin: string,
   expoTokenFromLogin: string
@@ -72,7 +88,13 @@ export async function sendNotifyTokenAPI(
   return testData;
 }
 
-// Get notification history
+/**
+ * This method will get the notification history of a certain user through the backend.
+ *
+ * @param userId : user that we're searching for notifications for.
+ * @returns null if error
+ *          An array of notifications resembling notification history
+ */
 export async function getNotificationHistoryAPI(userId: string): Promise<Notification[] | null> {
   const apiLinkWithExtension: string =
     ServerUrl.apiLink + ServerUrl.getNotificationHistoryUrlExtension(userId);

b90eea65df202b9d59882496b1a9b752023e136d
latanam <latanam@wwu.edu>
2022-10-02 01:04:32 -0700

    added group api service (need to add json parser)

--- app/human-detector-app/__tests__/BackendService.test.ts
+++ app/human-detector-app/__tests__/BackendService.test.ts
@@ -1,6 +1,8 @@
 import * as BackendService from '../services/BackendService';
 import * as ServerConfig from '../config/ServerConfig';
 import User from '../classes/User';
+import Group from '../classes/Group';
+import Camera from '../classes/Camera';
 
 const nock = require('nock');
 
@@ -10,7 +12,7 @@ describe(BackendService.sendNotifyTokenAPI, () => {
     const newUser: User = new User('tucker', '987654', true);
     const scope = nock(ServerConfig.apiLink)
       .put(ServerConfig.getSendNotifKeyUrlExtension(newUser.userID))
-      .reply(200);
+      .reply(200, 'success');
 
     const result: Promise<string> = await BackendService.sendNotifyTokenAPI(
       newUser.userID,
@@ -24,7 +26,7 @@ describe(BackendService.sendNotifyTokenAPI, () => {
     const newUser: User = new User('tucker', '987654', true);
     const scope = nock(ServerConfig.apiLink)
       .put(ServerConfig.getSendNotifKeyUrlExtension(newUser.userID))
-      .reply(401);
+      .reply(401, 'fail');
 
     const result = await BackendService.sendNotifyTokenAPI(
       newUser.userID,
@@ -32,5 +34,41 @@ describe(BackendService.sendNotifyTokenAPI, () => {
     );
 
     expect(result).toBe('401');
+  });
+});
+
+describe(BackendService.getGroupListAPI, () => {
+  const newUser: User = new User('bigheadgeorge', '1234565', true);
+
+  it('should return null if there is an error', async () => {
+    const scope = nock(ServerConfig.apiLink)
+      .get(ServerConfig.getGroupsListUrlExtension(newUser.userID))
+      .reply(401, 'error');
+
+    const result = await BackendService.getGroupListAPI(newUser.userID);
+
+    expect(result).toBe(null);
+  });
+
+  it('should return list of groups when successful', async () => {
+    const groupObj: Group = new Group('Group 1', 'ID');
+    groupObj.cameras[0] = new Camera('0', 'Camera 1', 'ID');
+    groupObj.cameras[1] = new Camera('0', 'Camera 2', 'ID2');
+
+    const groupObj2: Group = new Group('Grosefsefup 1', 'ID333');
+    groupObj.cameras[0] = new Camera('789', 'Camesfsefera 1', 'ID444');
+    groupObj.cameras[1] = new Camera('456', 'Camefesfra 2', 'ID2555');
+
+    const groups: Array<Group> = [];
+    groups.push(groupObj);
+    groups.push(groupObj2);
+
+    const scope = nock(ServerConfig.apiLink)
+      .get(ServerConfig.getGroupsListUrlExtension(newUser.userID))
+      .reply(200, JSON.stringify(groups));
+
+    const result = await BackendService.getGroupListAPI(newUser.userID);
+    console.log(JSON.stringify(groups));
+    expect(result).toBe(groups);
   });
 });

6b16306324d83809f1fd0aeeab6fa9d58755c635
Avery Black <avery.c.black@gmail.com>
2022-10-02 17:02:52 -0700

    Add heartbeat thread

--- camera/src/eyeSocket.py
+++ camera/src/eyeSocket.py
@@ -1,16 +1,31 @@
+from threading import Thread
+from time import sleep
 import requests
 
-class ServerConnection:
-    def __init__(self, token, eyespy_api):
-        self.token = token
-        self.api = eyespy_api
+URL_API="api.averycb.net"
+
+def get_notif_url(id):
+    return f"/cameras/{id}/notifications"
+
+def get_heartbeat_url(id):
+    return f"/cameras/{id}/heartbeat"
+
+def send_heartbeat():
+    pass
+
+class Heartbeat:
+    def __init__(self, heartbeat_delay=5):
+        self.running = True
+        self.heartbeat_delay = heartbeat_delay
+        self.thread = Thread(self.update, args=())
     
-    def send_notification(self, frame):
-        pass
+    def stop(self):
+        if hasattr(self, 'thread'):
+            self.running = False
+            self.thread.join()
 
-    def send_snapshot(self, frame):
-        pass
-
-    def get_settings(self):
-        pass
+    def update(self):
+        while self.running:
+            send_heartbeat()
+            sleep(self.heartbeat_delay)    
     

902def85ab925e3cc994d0a41a548e108ed6a68f
latanam <latanam@wwu.edu>
2022-10-03 21:36:34 -0700

    changes for keycloak and added env variable for keycloak

---
+++ app/human-detector-app/app.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'human-detector',
+    version: '1.0.0',
+    extra: {
+      keycloakUrl: process.env.KEYCLOAK_URL,
+    },
+  };
--- app/human-detector-app/screens/LoginScreen.tsx
+++ app/human-detector-app/screens/LoginScreen.tsx
@@ -1,21 +1,21 @@
 import * as React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
-import * as WebBrowser from 'expo-web-browser';
+import { View, StyleSheet, Button } from 'react-native';
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
 import * as AuthSession from 'expo-auth-session';
+import Constants from 'expo-constants';
 
 export default function LoginScreen(): React.ReactElement {
-  const discovery = AuthSession.useAutoDiscovery('http://localhost:8080/realms/myrealm');
+  // eslint-disable-next-line no-unsafe-optional-chaining
+  const apiUrl: string = Constants.manifest?.extra?.keycloakUrl;
+
+  const discovery = AuthSession.useAutoDiscovery(`${apiUrl}/realms/myrealm`);
   console.log(discovery);
 
   const [request, response, promptAsync] = useAuthRequest(
     {
       clientId: 'myclient',
       scopes: ['openid'],
-      redirectUri: makeRedirectUri({
-        // For usage in bare and standalone
-        native: 'http://localhost:19006',
-      }),
+      redirectUri: makeRedirectUri({}),
     },
     discovery
   );
@@ -23,19 +23,21 @@ export default function LoginScreen(): React.ReactElement {
   React.useEffect(() => {
     if (response?.type === 'success') {
       const { code } = response.params;
+      console.log(code);
     }
   }, [response]);
+
   // registerNNPushToken(4044, 'bbBo3vbjuqXCetI9mvFYic');
   // console.log(DeviceInfo.getModel()); // it returns 'Simulator'
 
   return (
     <View>
-      <Button 
-      onPress={promptAsync}
-      title="Login with KeyCloak"
-      >
-        <Text>Test</Text>
-      </Button>
+      <Button
+        onPress={() => {
+          promptAsync();
+        }}
+        title="Login with KeyCloak"
+      />
     </View>
   );
 }

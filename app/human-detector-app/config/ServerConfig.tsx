2b640adf45439034c1d77965228d7fc0e1972115
latanam <latanam@wwu.edu>
2022-10-07 23:34:47 -0700

    removed exchange code url extension (scrapped endpoint)

--- app/human-detector-app/config/ServerConfig.tsx
+++ app/human-detector-app/config/ServerConfig.tsx
@@ -6,4 +6,3 @@ export const getSendNotifKeyUrlExtension = (userId: string): string =>
 export const getGroupsListUrlExtension = (userId: string): string => `/users/${userId}/groups`;
 export const getNotificationHistoryUrlExtension = (userId: string): string =>
   `/users/${userId}/notifications`;
-export const exchangeCodeForTokenUrlExtension = '/auth/userAccessToken';

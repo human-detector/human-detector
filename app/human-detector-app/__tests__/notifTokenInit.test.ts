a151c824df172ee04ab8e182e458be7f8ccaac97
latanam <latanam@wwu.edu>
2022-10-10 23:16:23 -0700

    rename refactoring

---
+++ app/human-detector-app/__tests__/notifTokenInit.test.ts
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import renderer from 'react-test-renderer';
+import Notification, { addNotification, sendNotification } from '../notification/Notification';
+
+// sendNotification()
+// this will be tested on the device

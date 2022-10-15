c9290ca0bb39a17ba1f228b0fa4d40a252720186
Carter-Nakayama <nakayac@wwu.edu>
2022-10-15 16:33:29 -0700

    Updated send notification to work with Expo and sending to user.

--- server/backend/src/cameras/cameras.service.ts
+++ server/backend/src/cameras/cameras.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { Collection, EntityRepository } from '@mikro-orm/core';
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { NotFoundError } from '../errors.types';
 import { Notification } from './notification.entity';
 import { Camera } from './camera.entity';
+import { Expo } from 'expo-server-sdk';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Injectable()
 export class CamerasService {
@@ -16,10 +20,34 @@ export class CamerasService {
 
   public async sendNotification(idCam: string): Promise<boolean> {
     /* TODO: verify that ID refers to a valid camera and add a notification to DB */
-    const cam = await this.cameraRepository.findOne({ id: idCam });
+    const cam = await this.cameraRepository.findOne(
+      { id: idCam },
+      { populate: ['group.user'] },
+    );
     if (cam === null) {
       throw new NotFoundError(`Camera with given ID does not exist.`);
     }
+    const expoClient = new Expo({ accessToken: process.env.EXPO_ACCESS_TOKEN });
+
+    const messages = [];
+    const expoToken = cam.group.user.expoToken;
+
+    if (!Expo.isExpoPushToken(expoToken)) {
+      console.error(`Push token ${expoToken} is not a valid Expo push token`);
+    }
+
+    messages.push({
+      to: expoToken,
+      sound: 'default',
+      body: 'This is a test notification',
+      data: { withSome: 'data' },
+    });
+    try {
+      const sent = await expoClient.sendPushNotificationsAsync(messages);
+    } catch (error) {
+      console.error(error);
+    }
+
     cam.notifications.add(new Notification());
     this.cameraRepository.flush();
     return true;

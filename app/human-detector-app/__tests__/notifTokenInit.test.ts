db99777bc62e44ffffb869b75af61483a8a74d61
tucker-moore <mooret25@wwu.edu>
2022-10-26 19:19:28 -0700

    use typed mock functions

--- app/human-detector-app/__tests__/notifTokenInit.test.ts
+++ app/human-detector-app/__tests__/notifTokenInit.test.ts
@@ -1,12 +1,45 @@
 import * as Notifications from 'expo-notifications';
+import { PermissionResponse, PermissionStatus } from 'expo-modules-core';
+import { NotificationPermissionsStatus } from 'expo-notifications';
 import * as helpers from '../src/helpers';
 import * as BackendService from '../services/backendService';
-import * as ServerConfig from '../config/ServerConfig';
-import { sendExpoNotifToken, getExponentPushToken } from '../src/notifications/notifTokenInit';
+import { sendExpoNotifToken } from '../src/notifications/notifTokenInit';
 import User from '../classes/User';
 
 // Allows mocking of expo-notifications
 jest.mock('expo-notifications');
+const mockGetPermissionsAsync = Notifications.getPermissionsAsync as jest.MockedFunction<
+  typeof Notifications.getPermissionsAsync
+>;
+const mockRequestPermissionsAsync = Notifications.requestPermissionsAsync as jest.MockedFunction<
+  typeof Notifications.getPermissionsAsync
+>;
+const mockGetExpoPushTokenAsync = Notifications.getExpoPushTokenAsync as jest.MockedFunction<
+  typeof Notifications.getExpoPushTokenAsync
+>;
+const mockSetNotificationChannelAsync =
+  Notifications.setNotificationChannelAsync as jest.MockedFunction<
+    typeof Notifications.setNotificationChannelAsync
+  >;
+
+jest.mock('../services/backendService');
+const mockSendNotifyToken = BackendService.sendNotifyTokenAPI as jest.MockedFunction<
+  typeof BackendService.sendNotifyTokenAPI
+>;
+
+jest.mock('../src/helpers');
+const mockGetOS = helpers.getOS as jest.MockedFunction<typeof helpers.getOS>;
+const mockIsDevice = helpers.isDevice as jest.MockedFunction<typeof helpers.isDevice>;
+
+// Generate a dummy permission response for testing
+function createPermissionResponse(status: PermissionStatus): PermissionResponse {
+  return {
+    status,
+    expires: 'never',
+    granted: status === PermissionStatus.GRANTED,
+    canAskAgain: true,
+  };
+}
 
 // Example user
 const user: User = new User('fjosejfoesf', '3232323', true);
@@ -15,7 +48,7 @@ const exampleAccessToken = 'ExampleAccessToken';
 
 // Permanent mocks
 beforeEach(() => {
-  helpers.isDevice = jest.fn(() => true);
+  mockIsDevice.mockReturnValue(true);
 });
 
 afterEach(() => {
@@ -23,15 +56,16 @@ afterEach(() => {
 });
 
 describe('sendExpoNotifToken() iOS', () => {
-  helpers.getOS = jest.fn(() => 'iOS');
+  mockGetOS.mockReturnValue('ios');
 
   it('will return void if the function is successful with getting the token ', async () => {
     // Success mocks
-    Notifications.getPermissionsAsync.mockResolvedValue({ status: 'granted' });
-    Notifications.getExpoPushTokenAsync.mockResolvedValue({
+    mockGetPermissionsAsync.mockResolvedValue(createPermissionResponse(PermissionStatus.GRANTED));
+    mockGetExpoPushTokenAsync.mockResolvedValue({
+      type: 'expo',
       data: 'ExponentPushToken[000000000000]',
     });
-    BackendService.sendNotifyTokenAPI = jest.fn(() => Promise.resolve());
+    mockSendNotifyToken.mockResolvedValueOnce();
 
     await sendExpoNotifToken(user.userID, exampleAccessToken);
     expect(helpers.isDevice).toHaveBeenCalledTimes(1);
@@ -48,12 +82,17 @@ describe('sendExpoNotifToken() iOS', () => {
 
   it('will return void if successful and user just turns on notifications', async () => {
     // Success mocks
-    Notifications.getPermissionsAsync.mockResolvedValue({ status: 'undetermined' });
-    Notifications.requestPermissionsAsync.mockResolvedValue({ status: 'granted' });
-    Notifications.getExpoPushTokenAsync.mockResolvedValue({
+    mockGetPermissionsAsync.mockResolvedValue(
+      createPermissionResponse(PermissionStatus.UNDETERMINED)
+    );
+    mockRequestPermissionsAsync.mockResolvedValue(
+      createPermissionResponse(PermissionStatus.GRANTED)
+    );
+    mockGetExpoPushTokenAsync.mockResolvedValue({
+      type: 'expo',
       data: 'ExponentPushToken[000000000000]',
     });
-    BackendService.sendNotifyTokenAPI = jest.fn(() => Promise.resolve());
+    mockSendNotifyToken.mockResolvedValueOnce();
 
     await sendExpoNotifToken(user.userID, exampleAccessToken);
     expect(helpers.isDevice).toHaveBeenCalledTimes(1);
@@ -65,8 +104,12 @@ describe('sendExpoNotifToken() iOS', () => {
 
   it('will return a reject if user just turns off notifications', async () => {
     // Success mocks
-    Notifications.getPermissionsAsync.mockResolvedValue({ status: 'undetermined' });
-    Notifications.requestPermissionsAsync.mockResolvedValue({ status: 'denied' });
+    mockGetPermissionsAsync.mockResolvedValue(
+      createPermissionResponse(PermissionStatus.UNDETERMINED)
+    );
+    mockRequestPermissionsAsync.mockResolvedValue({
+      status: PermissionStatus.DENIED,
+    } as NotificationPermissionsStatus);
 
     const errorMsg = 'Failed to get push token for push notification!';
     await expect(sendExpoNotifToken(user.userID, exampleAccessToken)).rejects.toStrictEqual(
@@ -75,9 +118,12 @@ describe('sendExpoNotifToken() iOS', () => {
   });
 
   it('will return a reject if notifications are off', async () => {
-    Notifications.getPermissionsAsync.mockResolvedValue({ status: 'denied' }); // User turned off notifications
-    Notifications.requestPermissionsAsync.mockResolvedValue({ status: 'denied' }); // User presses no to notifiations
-    Notifications.getExpoPushTokenAsync.mockResolvedValue({
+    mockGetPermissionsAsync.mockResolvedValue(createPermissionResponse(PermissionStatus.DENIED)); // User turned off notifications
+    mockRequestPermissionsAsync.mockResolvedValue(
+      createPermissionResponse(PermissionStatus.DENIED)
+    ); // User presses no to notifiations
+    mockGetExpoPushTokenAsync.mockResolvedValue({
+      type: 'expo',
       data: 'ExponentPushToken[000000000000]',
     });
 
@@ -88,10 +134,8 @@ describe('sendExpoNotifToken() iOS', () => {
   });
 
   it('will return a reject with error message error in sending to server', async () => {
-    Notifications.getPermissionsAsync.mockResolvedValue({ status: 'granted' });
-    BackendService.sendNotifyTokenAPI = jest.fn(() =>
-      Promise.reject(new Error('Placeholder Error'))
-    ); // Sending to server error
+    mockGetPermissionsAsync.mockResolvedValue(createPermissionResponse(PermissionStatus.GRANTED));
+    mockSendNotifyToken.mockRejectedValueOnce(new Error('Placeholder Error')); // Sending to server error
 
     const errorMsg = 'Placeholder Error';
     await expect(sendExpoNotifToken(user.userID, exampleAccessToken)).rejects.toStrictEqual(
@@ -100,10 +144,8 @@ describe('sendExpoNotifToken() iOS', () => {
   });
 
   it('will return a reject if there is an error with getting the ExpoPushToken from the user', async () => {
-    Notifications.getPermissionsAsync.mockResolvedValue({ status: 'granted' });
-    Notifications.getExpoPushTokenAsync = jest.fn(() =>
-      Promise.reject(new Error('Placeholder Error for Test 4'))
-    ); // Getting from the user error
+    mockGetPermissionsAsync.mockResolvedValue(createPermissionResponse(PermissionStatus.GRANTED));
+    mockGetExpoPushTokenAsync.mockRejectedValueOnce(new Error('Placeholder Error for Test 4')); // Getting from the user error
 
     const errorMsg = 'Placeholder Error for Test 4';
     await expect(sendExpoNotifToken(user.userID, exampleAccessToken)).rejects.toStrictEqual(
@@ -113,15 +155,16 @@ describe('sendExpoNotifToken() iOS', () => {
 });
 
 describe('sendExpoNotifToken() android', () => {
-  helpers.getOS = jest.fn(() => 'android');
+  mockGetOS.mockReturnValue('android');
   it('will return void if the function is successful with getting the token ', async () => {
     // Success mocks
-    Notifications.getPermissionsAsync.mockResolvedValue({ status: 'granted' });
-    Notifications.setNotificationChannelAsync = jest.fn(() => Promise.resolve());
-    Notifications.getExpoPushTokenAsync.mockResolvedValue({
+    mockGetPermissionsAsync.mockResolvedValue(createPermissionResponse(PermissionStatus.GRANTED));
+    mockSetNotificationChannelAsync.mockResolvedValueOnce(null);
+    mockGetExpoPushTokenAsync.mockResolvedValue({
+      type: 'expo',
       data: 'ExponentPushToken[000000000000]',
     });
-    BackendService.sendNotifyTokenAPI = jest.fn(() => Promise.resolve());
+    mockSendNotifyToken.mockResolvedValueOnce();
 
     await sendExpoNotifToken(user.userID, exampleAccessToken);
     expect(helpers.isDevice).toHaveBeenCalledTimes(1);

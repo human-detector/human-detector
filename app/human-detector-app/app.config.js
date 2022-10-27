d9e32d4495b7ad453325ce652dd1a7e2ed394430
latanam <latanam@wwu.edu>
2022-10-27 03:21:12 -0700

    update for eas

--- app/human-detector-app/app.config.js
+++ app/human-detector-app/app.config.js
@@ -11,7 +11,20 @@ module.exports = {
       }
     },
     scheme: 'myapp',
-    "plugins": ["@config-plugins/react-native-ble-plx"],
+    "plugins": ["@config-plugins/react-native-ble-plx",
+    [
+      "expo-build-properties",
+      {
+        "android": {
+          "compileSdkVersion": 33,
+          "targetSdkVersion": 33,
+          "buildToolsVersion": "33.0.0"
+        },
+        "ios": {
+          "deploymentTarget": "13.0"
+        }
+      }
+    ]],
     "ios": {
       "bundleIdentifier": "com.yourcompany.yourappname",
       "supportsTablet": true,

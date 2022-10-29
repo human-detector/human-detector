4361a3e97a8c8766cfad51f71535223165381b87
tucker-moore <mooret25@wwu.edu>
2022-10-29 01:29:01 -0700

    fix tests

--- app/human-detector-app/__tests__/BackendService.test.ts
+++ app/human-detector-app/__tests__/BackendService.test.ts
@@ -1,6 +1,7 @@
 import nock from 'nock';
 import { AxiosError } from 'axios';
-import * as BackendService from '../services/backendService';
+import BackendService from '../services/backendService';
+import TokenManager from '../src/auth/tokenManager';
 import * as ServerConfig from '../config/ServerConfig';
 import User from '../classes/User';
 import Group from '../classes/Group';
@@ -9,106 +10,116 @@ import Notification from '../classes/Notification';
 
 const exampleAccessToken = 'ExampleAccessToken';
 
-// test putting notification key
-describe(BackendService.sendNotifyTokenAPI, () => {
-  it('should return code 200 for successful token sending', async () => {
+describe(BackendService, () => {
+  // I want to use 'jest-ts-auto-mock', but Expo won't let me! :)))
+  const mockTokenManager: jest.Mocked<TokenManager> = {
+    getAccessToken: jest.fn(),
+    getUser: jest.fn(),
+  } as unknown as jest.Mocked<TokenManager>;
+
+  beforeEach(() => {
+    mockTokenManager.getAccessToken.mockResolvedValue(exampleAccessToken);
+    mockTokenManager.getUser.mockClear();
+  });
+
+  // test putting notification key
+  describe('sendNotifyTokenAPI', () => {
+    const backendService = new BackendService(mockTokenManager);
     const newUser: User = new User('tucker', '987654', true);
-    nock(ServerConfig.apiLink)
-      .put(ServerConfig.getSendNotifKeyUrlExtension(newUser.userID))
-      .reply(200, 'success');
 
-    await BackendService.sendNotifyTokenAPI(
-      newUser.userID,
-      'ExponentPushToken[0000000000]',
-      exampleAccessToken
-    );
+    it('should return code 200 for successful token sending', async () => {
+      mockTokenManager.getUser.mockReturnValue(newUser);
+      nock(ServerConfig.apiLink)
+        .put(ServerConfig.getSendNotifKeyUrlExtension(newUser.userID))
+        .reply(200, 'success');
+
+      await backendService.sendNotifyTokenAPI('ExponentPushToken[0000000000]');
+    });
+
+    it("should return code 401 if user id doesn't exist", async () => {
+      mockTokenManager.getUser.mockReturnValue(newUser);
+      nock(ServerConfig.apiLink)
+        .put(ServerConfig.getSendNotifKeyUrlExtension(newUser.userID))
+        .reply(401, 'fail');
+
+      await expect(
+        backendService.sendNotifyTokenAPI('ExponentPushToken[0000000000]')
+      ).rejects.toThrow(AxiosError);
+    });
   });
 
-  it("should return code 401 if user id doesn't exist", async () => {
-    const newUser: User = new User('tucker', '987654', true);
-    nock(ServerConfig.apiLink)
-      .put(ServerConfig.getSendNotifKeyUrlExtension(newUser.userID))
-      .reply(401, 'fail');
+  describe('getGroupListAPI', () => {
+    const backendService = new BackendService(mockTokenManager);
+    const newUser: User = new User('bigheadgeorge', '0', true);
 
-    await expect(
-      BackendService.sendNotifyTokenAPI(
-        newUser.userID,
-        'ExponentPushToken[0000000000]',
-        exampleAccessToken
-      )
-    ).rejects.toThrow(AxiosError);
-  });
-});
+    it('should return null if there is an error', async () => {
+      mockTokenManager.getUser.mockReturnValue(newUser);
+      nock(ServerConfig.apiLink)
+        .get(ServerConfig.getGroupsListUrlExtension(newUser.userID))
+        .reply(401, 'error');
 
-describe(BackendService.getGroupListAPI, () => {
-  const newUser: User = new User('bigheadgeorge', '0', true);
+      const result = await backendService.getGroupListAPI();
 
-  it('should return null if there is an error', async () => {
-    nock(ServerConfig.apiLink)
-      .get(ServerConfig.getGroupsListUrlExtension(newUser.userID))
-      .reply(401, 'error');
+      expect(result).toBe(null);
+    });
 
-    const result = await BackendService.getGroupListAPI(newUser.userID, exampleAccessToken);
+    it('should return list of groups when successful', async () => {
+      mockTokenManager.getUser.mockReturnValue(newUser);
 
-    expect(result).toBe(null);
-  });
+      const groupObj: Group = new Group('Group 1', 'ID');
+      groupObj.cameras[0] = new Camera('0', 'Camera 1', 'ID');
+      groupObj.cameras[1] = new Camera('0', 'Camera 2', 'ID2');
 
-  it('should return list of groups when successful', async () => {
-    const groupObj: Group = new Group('Group 1', 'ID');
-    groupObj.cameras[0] = new Camera('0', 'Camera 1', 'ID');
-    groupObj.cameras[1] = new Camera('0', 'Camera 2', 'ID2');
+      const groupObj2: Group = new Group('Grosefsefup 1', 'ID333');
+      groupObj2.cameras[0] = new Camera('0', 'Camesfsefera 1', 'ID444');
+      groupObj2.cameras[1] = new Camera('0', 'Camefesfra 2', 'ID2555');
 
-    const groupObj2: Group = new Group('Grosefsefup 1', 'ID333');
-    groupObj2.cameras[0] = new Camera('0', 'Camesfsefera 1', 'ID444');
-    groupObj2.cameras[1] = new Camera('0', 'Camefesfra 2', 'ID2555');
+      const groups: Array<Group> = [];
+      groups.push(groupObj);
+      groups.push(groupObj2);
 
-    const groups: Array<Group> = [];
-    groups.push(groupObj);
-    groups.push(groupObj2);
+      nock(ServerConfig.apiLink)
+        .get(ServerConfig.getGroupsListUrlExtension(newUser.userID))
+        .reply(200, JSON.stringify(groups));
 
-    nock(ServerConfig.apiLink)
-      .get(ServerConfig.getGroupsListUrlExtension(newUser.userID))
-      .reply(200, JSON.stringify(groups));
+      const result = await backendService.getGroupListAPI();
 
-    const result = await BackendService.getGroupListAPI(newUser.userID, exampleAccessToken);
-
-    expect(result).toEqual(groups);
-  });
-});
-
-describe(BackendService.getNotificationHistoryAPI, () => {
-  const newUser: User = new User('bigheadgeorge', '1234565', true);
-
-  it('should return null if there is an error', async () => {
-    nock(ServerConfig.apiLink)
-      .get(ServerConfig.getNotificationHistoryUrlExtension(newUser.userID))
-      .reply(401, 'error');
-
-    const result = await BackendService.getNotificationHistoryAPI(
-      newUser.userID,
-      exampleAccessToken
-    );
-
-    expect(result).toBe(null);
+      expect(result).toEqual(groups);
+    });
   });
 
-  it('should return notification history of the current user when theres a valid notification history', async () => {
-    // make notification array
-    const cam: Camera = new Camera('9999', 'testCamera', '29292');
-    const notif1: Notification = new Notification('2022-06-14T03:24:00', cam);
-    const notif2: Notification = new Notification('2007-12-27T00:00:00', cam);
+  describe('getNotificationHistoryAPI', () => {
+    const backendService = new BackendService(mockTokenManager);
+    const newUser: User = new User('bigheadgeorge', '1234565', true);
 
-    const notifHistory: Notification[] = [notif1, notif2];
+    it('should return null if there is an error', async () => {
+      mockTokenManager.getUser.mockReturnValue(newUser);
+      nock(ServerConfig.apiLink)
+        .get(ServerConfig.getNotificationHistoryUrlExtension(newUser.userID))
+        .reply(401, 'error');
 
-    nock(ServerConfig.apiLink)
-      .get(ServerConfig.getNotificationHistoryUrlExtension(newUser.userID))
-      .reply(200, JSON.stringify(notifHistory));
+      const result = await backendService.getNotificationHistoryAPI();
 
-    const result = await BackendService.getNotificationHistoryAPI(
-      newUser.userID,
-      exampleAccessToken
-    );
+      expect(result).toBe(null);
+    });
 
-    expect(result).toEqual(notifHistory);
+    it('should return notification history of the current user when theres a valid notification history', async () => {
+      mockTokenManager.getUser.mockReturnValue(newUser);
+
+      // make notification array
+      const cam: Camera = new Camera('9999', 'testCamera', '29292');
+      const notif1: Notification = new Notification('2022-06-14T03:24:00', cam);
+      const notif2: Notification = new Notification('2007-12-27T00:00:00', cam);
+
+      const notifHistory: Notification[] = [notif1, notif2];
+
+      nock(ServerConfig.apiLink)
+        .get(ServerConfig.getNotificationHistoryUrlExtension(newUser.userID))
+        .reply(200, JSON.stringify(notifHistory));
+
+      const result = await backendService.getNotificationHistoryAPI();
+
+      expect(result).toEqual(notifHistory);
+    });
   });
 });

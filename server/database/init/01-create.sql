27c2c3ed3f1b9ef195eee7d282c5ae26d1901e26
tucker-moore <mooret25@wwu.edu>
2022-11-12 18:52:36 -0800

    update database schema

--- server/database/init/01-create.sql
+++ server/database/init/01-create.sql
@@ -5,14 +5,18 @@ create table "user" ("id" uuid not null, "expo_token" varchar(255) null, constra
 
 create table "group" ("id" uuid not null, "name" varchar(255) not null, "user_id" uuid not null, constraint "group_pkey" primary key ("id"));
 
-create table "camera" ("id" uuid not null, "name" varchar(255) not null, "publicKey" varchar(255) not null, "serial" varchar(255) not null, "group_id" uuid not null, constraint "camera_pkey" primary key ("id"));
+create table "camera" ("id" uuid not null, "name" varchar(255) not null, "public_key" varchar(255) not null, "serial" varchar(255) not null, "group_id" uuid not null, constraint "camera_pkey" primary key ("id"));
 
-create table "notification" ("id" uuid not null, "timestamp" timestamptz(0) not null, "camera_id" uuid not null, constraint "notification_pkey" primary key ("id"));
+create table "snapshot" ("id" uuid not null, "image" bytea not null, constraint "snapshot_pkey" primary key ("id"));
+
+create table "notification" ("id" uuid not null, "timestamp" timestamptz(0) not null, "camera_id" uuid not null, "snapshot_id" uuid not null, constraint "notification_pkey" primary key ("id"));
+alter table "notification" add constraint "notification_snapshot_id_unique" unique ("snapshot_id");
 
 alter table "group" add constraint "group_user_id_foreign" foreign key ("user_id") references "user" ("id") on update cascade;
 
 alter table "camera" add constraint "camera_group_id_foreign" foreign key ("group_id") references "group" ("id") on update cascade;
 
 alter table "notification" add constraint "notification_camera_id_foreign" foreign key ("camera_id") references "camera" ("id") on update cascade;
+alter table "notification" add constraint "notification_snapshot_id_foreign" foreign key ("snapshot_id") references "snapshot" ("id") on update cascade;
 
 set session_replication_role = 'origin';

8c01c255cb9f7c856029c894760637f9857d35a3
latanam <latanam@wwu.edu>
2023-01-09 19:18:57 -0800

    we zooming

--- app/human-detector-app/screens/SnapshotScreen.tsx
+++ app/human-detector-app/screens/SnapshotScreen.tsx
@@ -1,6 +1,7 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { View, Image, ImageURISource } from 'react-native';
+import { View, Image, ImageURISource, Dimensions } from 'react-native';
+import ImageZoom from 'react-native-image-pan-zoom';
 import { LoadingIcon, LoadingState } from '../components/LoadingIcon';
 import { BackendContext } from '../contexts/backendContext';
 import { RootStackParamList } from '../src/navigation/stackParamList';
@@ -26,7 +27,18 @@ export default function SnapshotScreen({ route }: Props): React.ReactElement<Pro
 
   return (
     <View>
-      <Image style={{ width: '100%', height: '100%' }} source={snapshotUrl} resizeMode="contain" />
+      <ImageZoom
+        cropWidth={Dimensions.get('window').width}
+        cropHeight={Dimensions.get('window').height}
+        imageWidth={Dimensions.get('window').width}
+        imageHeight={Dimensions.get('window').height}
+      >
+        <Image
+          style={{ width: '100%', height: '100%' }}
+          source={snapshotUrl}
+          resizeMode="contain"
+        />
+      </ImageZoom>
     </View>
   );
 }

7158d2878efa93c97bf5cfe32d7bebd09c0fb487
Avery Black <avery.c.black@gmail.com>
2023-01-11 19:50:55 -0800

    Retry making new camera on error (#62)
    * Cursed
    
    * Reset camera on error
    
    * Loop until valid image

--- camera/detector/src/image_sources/camera_source.py
+++ camera/detector/src/image_sources/camera_source.py
@@ -1,9 +1,9 @@
 from distutils.log import error
 import cv2
+import time
 
 class NoCameraException(Exception):
     """Raised when a camera could not be opened"""
-    pass
 
 class RuntimeCameraException(Exception):
     """Raised when the camera returns a failure trying to capture a frame"""
@@ -11,6 +11,8 @@ class RuntimeCameraException(Exception):
 class CameraSource:
     def __init__(self, resolution=(1920, 1080), fps=30):
         self.camera = self.open_camera(resolution, fps)
+        self.resolution = resolution
+        self.fps = fps
     
     def __del__(self):
         self.running = False
@@ -20,8 +22,9 @@ class CameraSource:
 
         # Open the default webcam (Raspberry Pi should only have one)
         camera = cv2.VideoCapture(0)
-        if not camera.isOpened():
-            raise NoCameraException
+        while(not camera.isOpened()):
+            time.sleep(5) 
+            camera = cv2.VideoCapture(0)
 
         # Webcams usually give higher framerate options using MJPG vs other standards
         camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc("M", "J", "P", "G"))
@@ -36,7 +39,9 @@ class CameraSource:
     def get_frame(self):
         success, frame = self.camera.read()
 
-        if not success:
-            raise RuntimeCameraException
+        while not success:
+            self.camera.release()
+            self.camera = self.open_camera(self.resolution, self.fps)
+            success, frame = self.camera.read()
         
         return frame
